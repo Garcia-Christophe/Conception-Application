5702963637ea0857f9a6aa07df5b48d902675a9a
Garcia-Christophe <garciachristophe29@gmail.com>
2022-01-15 12:11:26 +0100

    Gestion : gestion membre

--- CLAMM/src/gestion/Gestion.java
+++ CLAMM/src/gestion/Gestion.java
@@ -8,6 +8,9 @@ import gestionMembres.Membre;
 
 public class Gestion {
 
+  /**
+   * Liste des codes erreurs
+   */
   private ArrayList<CodeErreur> codesErreurs;
 
   /**
@@ -26,12 +29,12 @@ public class Gestion {
   private ArrayList<Membre> listeMembres;
 
   /**
-   * Constructeur de l'interface Gestion
+   * Constructeur de la classe {@code Gestion}
    */
   public Gestion() {
     this.setListeMembres(new ArrayList<Membre>());
-    this.setListeEvenements(new ArrayList<>());
-    this.codesErreurs = new ArrayList<>();
+    this.setListeEvenements(new ArrayList<Evenement>());
+    this.setCodesErreurs(new ArrayList<CodeErreur>());
     this.nextId = 1;
   }
 
@@ -40,6 +43,22 @@ public class Gestion {
    */
   public ArrayList<Evenement> getListeEvenements() {
     return listeEvenements;
+  }
+
+  /**
+   * @param uneListe La liste des codes erreurs
+   */
+  private void setCodesErreurs(ArrayList<CodeErreur> uneListe) {
+    if (uneListe != null) {
+      this.codesErreurs = uneListe;
+    }
+  }
+
+  /**
+   * @return la liste des codes erreurs
+   */
+  public ArrayList<CodeErreur> getCodesErreurs() {
+    return this.codesErreurs;
   }
 
   /**
@@ -222,7 +241,20 @@ public class Gestion {
   }
 
   /**
-   * Crée un membre.
+   * Crée un {@code Membre}.
+   * 
+   * <p>
+   * Si une ou plus définition(s) des attributs du nouveau membre est un échec, ajoute le code
+   * erreur dans la liste des codes erreurs, et retourne un membre {@code null}.
+   * 
+   * <p>
+   * Les codes erreurs sont les suivants :
+   * <ul>
+   * <li>- PSEUDO_TROP_COURT : lorsque la taille du pseudo est trop petite</li>
+   * <li>- PSEUDO_TROP_LONG : lorsque la taille du pseudo est trop grande</li>
+   * <li>- PSEUDO_CARACTERE_INVALIDE : lorsqu'un caractère du pseudo n'est pas possible</li>
+   * <li>- NOM_TROP_LONG : lorsque le nom est trop long</li>
+   * <li>- ...</li>
    * 
    * @param unPseudo pseudo du membre à créer
    * @param unNom nom du membre à créer
@@ -232,27 +264,65 @@ public class Gestion {
    * @param uneVille ville du membre à créer
    * @param unMail adresse mail du membre à créer
    * @param unMotDePasse mot de passe du membre à créer
-   * @return une instance de la classe {@code Membre} la création est un succès, {@code null} sinon
+   * @return une nouvelle instance de la classe {@code Membre} si la création est un succès,
+   *         {@code null} sinon
    */
   private Membre creerMembre(String unPseudo, String unNom, String unPrenom, String unLieuNaissance,
       Date uneDateNaissance, String uneVille, String unMail, String unMotDePasse) {
     Membre membre = new Membre();
-    int res = 0;
+    this.codesErreurs.clear();
+    CodeErreur codeErreur = null;
 
-    // Définition des attributs du membre
-    res += membre.setPseudo(unPseudo);
-    res += membre.setNom(unNom);
-    res += membre.setPrenom(unPrenom);
-    res += membre.setLieuNaissance(unLieuNaissance);
-    res += membre.setDateNaissance(uneDateNaissance);
-    res += membre.setVille(uneVille);
-    res += membre.setMail(unMail);
-    res += membre.setMotDePasse(unMotDePasse);
+    // Définition du nouveau pseudo du membre
+    codeErreur = membre.setPseudo(unPseudo);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
 
-    // recup membre.getErreurs();
+    // Définition du nouveau nom du membre
+    codeErreur = membre.setNom(unNom);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
+
+    // Définition du nouveau prénom du membre
+    codeErreur = membre.setPrenom(unPrenom);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
+
+    // Définition du nouveau lieu de naissance du membre
+    codeErreur = membre.setLieuNaissance(unLieuNaissance);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
+
+    // Définition de la nouvelle date de naissance du membre
+    codeErreur = membre.setDateNaissance(uneDateNaissance);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
+
+    // Définition de la nouvelle ville du membre
+    codeErreur = membre.setVille(uneVille);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
+
+    // Définition de la nouvelle adresse mail du membre
+    codeErreur = membre.setMail(unMail);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
+
+    // Définition du nouveau mot de passe du membre
+    codeErreur = membre.setMotDePasse(unMotDePasse);
+    if (codeErreur != null) {
+      this.codesErreurs.add(codeErreur);
+    }
 
     // Si une des définitions est un échec, retourne un membre null
-    if (res != 0) {
+    if (!this.codesErreurs.isEmpty()) {
       membre = null;
     }
 
@@ -260,106 +330,192 @@ public class Gestion {
   }
 
   /**
-   * Ajoute un membre à la liste des membres, s'il n'existe pas déjà.
+   * Ajoute un {@code Membre} à la liste des membres, seulement s'il n'existe pas déjà.
    * 
-   * @param unPseudo pseudo du membre à ajouter
-   * @param unNom nom du membre à ajouter
-   * @param unPrenom prénom du membre à ajouter
-   * @param unLieuNaissance lieu de naissance du membre à ajouter
-   * @param uneDateNaissance date de naissance du membre à ajouter
-   * @param uneVille ville du membre à ajouter
-   * @param unMail adresse mail du membre à ajouter
-   * @param unMotDePasse mot de passe du membre à ajouter
-   * @return {@code 0} si l'ajout est un succès, {@code -1} sinon
+   * <p>
+   * Avec la méthode {@link #getMembre(String)}, on récupère le membre déjà existant dans la liste
+   * des membres à partir du pseudo {@code unPseudo}. Si la valeur n'est pas {@code null}, alors le
+   * code erreur {@code CodeErreur.PSEUDO_DEJA_EXISTANT} est renvoyé. Sinon, un appel à
+   * {@link #creerMembre(String, String, String, String, Date, String, String, String)} est réalisé
+   * pour créer le membre avant de l'ajouter dans la liste des membres.
+   * 
+   * @param unPseudo pseudo du nouveau membre
+   * @param unNom nom du nouveau membre
+   * @param unPrenom prénom du nouveau membre
+   * @param unLieuNaissance lieu de naissance du nouveau membre
+   * @param uneDateNaissance date de naissance du nouveau membre
+   * @param uneVille ville du nouveau membre
+   * @param unMail adresse mail du nouveau membre
+   * @param unMotDePasse mot de passe du nouveau membre
+   * @return {@code null} si l'ajout du nouveau membre est un succès, une liste de
+   *         {@code CodeErreur} sinon
    */
-  public int ajouterMembre(String unPseudo, String unNom, String unPrenom, String unLieuNaissance,
-      Date uneDateNaissance, String uneVille, String unMail, String unMotDePasse) {
-    int res = -1;
-    Membre membreExistant = this.getMembre(unPseudo);
+  public ArrayList<CodeErreur> ajouterMembre(String unPseudo, String unNom, String unPrenom,
+      String unLieuNaissance, Date uneDateNaissance, String uneVille, String unMail,
+      String unMotDePasse) {
+    ArrayList<CodeErreur> res = null;
+    Membre membre = this.getMembre(unPseudo);
 
-    // Si le pseudo du membre à ajouter n'existe pas
-    if (membreExistant == null) {
-      Membre membre = this.creerMembre(unPseudo, unNom, unPrenom, unLieuNaissance, uneDateNaissance,
+    // Si aucun membre de la liste des membres ne possède le même pseudo
+    if (membre == null) {
+      membre = this.creerMembre(unPseudo, unNom, unPrenom, unLieuNaissance, uneDateNaissance,
           uneVille, unMail, unMotDePasse);
 
       // Si la création du membre est un succès
       if (membre != null) {
-        this.listeMembres.add(membre);
-        res = 0;
+        this.listeMembres.add(membre); // ajoute le membre à la liste des membres
+      } else {
+        res = this.getCodesErreurs(); // renvoie la liste des codes erreurs
       }
+    } else {
+      this.codesErreurs.clear();
+      this.codesErreurs.add(CodeErreur.PSEUDO_DEJA_EXISTANT);
     }
 
     return res;
   }
 
   /**
-   * Supprime un membre de la liste des membres.
+   * Supprime un {@code Membre} de la liste des membres.
+   * 
+   * <p>
+   * Cherche le membre ayant pour pseudo {@code unPseudo} dans la liste des membres avec la méthode
+   * {@link #getMembre(String)}. Si le membre est trouvé, alors le retire de la liste des membres et
+   * ne renvoie aucun code erreur, renvoie {@code null}. Si aucun membre n'est trouvé dans la liste
+   * des membres, alors renvoie le code erreur {@code CodeErreur.MEMBRE_INTROUVABLE}.
    * 
    * @param unPseudo pseudo du membre à supprimer de la liste
-   * @return {@code 0} si la suppression est un succès, {@code -1} sinon
+   * @return {@code null} si la suppression du membre est un succès, une liste de {@code CodeErreur}
+   *         sinon
    */
-  public int supprimerMembre(String unPseudo) {
-    int res = -1;
+  public ArrayList<CodeErreur> supprimerMembre(String unPseudo) {
+    ArrayList<CodeErreur> res = null;
+    Membre membreASupprimer = this.getMembre(unPseudo);
 
-    // Si le paramètre n'est pas null
-    if (unPseudo != null) {
-      Membre membre = this.getMembre(unPseudo);
-
-      // Si le membre est dans la liste des membres
-      if (membre != null) {
-        // Supprime le membre de la liste
-        this.listeMembres.remove(membre);
-        res = 1;
-      }
+    // Si le membre est dans la liste des membres
+    if (membreASupprimer != null) {
+      this.listeMembres.remove(membreASupprimer); // supprime le membre de la liste
+    } else {
+      this.codesErreurs.clear();
+      this.codesErreurs.add(CodeErreur.MEMBRE_INTROUVABLE);
     }
 
     return res;
   }
 
   /**
-   * Modifie les données d'un membre de la liste des membres.
+   * Modifie les données d'un membre de la liste des membres, si et seulement si toutes les
+   * modifications sont des succès.
    * 
-   * @param unMembre le membre de la liste à modifier
-   * @return {@code 0} si la modification est un succès, {@code -1} sinon
+   * <p>
+   * Cherche le membre ayant pour pseudo {@code unPseudo} dans la liste des membres avec la méthode
+   * {@link #getMembre(String)}. Si aucun membre n'est trouvé dans la liste des membres, alors
+   * renvoie le code erreur {@code CodeErreur.MEMBRE_INTROUVABLE}. Si le membre est trouvé, alors
+   * tente de modifier ses attributs. Si tous les attributs du membre ont été modifiés avec succès,
+   * ne renvoie aucun code erreur, renvoie {@code null}, sinon renvoie la liste des codes erreurs
+   * correspondantes.
+   * 
+   * <p>
+   * Les codes erreurs dus aux définitions sont les suivants :
+   * <ul>
+   * <li>- PSEUDO_TROP_COURT : lorsque la taille du pseudo est trop petite</li>
+   * <li>- PSEUDO_TROP_LONG : lorsque la taille du pseudo est trop grande</li>
+   * <li>- PSEUDO_CARACTERE_INVALIDE : lorsqu'un caractère du pseudo n'est pas possible</li>
+   * <li>- NOM_TROP_LONG : lorsque le nom est trop long</li>
+   * <li>- ...</li>
+   * 
+   * @param unPseudo nouveau pseudo du membre
+   * @param unNom nouveau nom du membre
+   * @param unPrenom nouveau prénom du membre
+   * @param unLieuNaissance nouveua lieu de naissance du membre
+   * @param uneDateNaissance nouvelle date de naissance du membre
+   * @param uneVille nouvelle ville du membre
+   * @param unMail nouvelle adresse mail du membre
+   * @param unMotDePasse nouveau mot de passe du membre
+   * @return {@code null} si la modification du membre est un succès, une liste de
+   *         {@code CodeErreur} sinon
    */
-  public int modifierMembre(Membre unMembre) {
-    int res = -1;
+  public ArrayList<CodeErreur> modifierMembre(String unPseudo, String unNom, String unPrenom,
+      String unLieuNaissance, Date uneDateNaissance, String uneVille, String unMail,
+      String unMotDePasse) {
+    ArrayList<CodeErreur> res = null;
+    Membre membreAModifier = this.getMembre(unPseudo);
+    this.codesErreurs.clear();
 
-    // Si le paramètre n'est pas null
-    if (unMembre != null) {
-      Membre membreAModifier = this.getMembre(unMembre.getPseudo());
+    // Si le membre est dans la liste des membres
+    if (membreAModifier != null) {
+      CodeErreur codeErreur = null;
 
-      // Si le membre est dans la liste des membres
-      if (membreAModifier != null) {
-
-        // Modifie le membre de la liste
-        int date = membreAModifier.setDateNaissance(unMembre.getDateNaissance());
-        int lieu = membreAModifier.setLieuNaissance(unMembre.getLieuNaissance());
-        int mail = membreAModifier.setMail(unMembre.getLieuNaissance());
-        int mdp = membreAModifier.setMotDePasse(unMembre.getMotDePasse());
-        int nom = membreAModifier.setNom(unMembre.getNom());
-        int prenom = membreAModifier.setPrenom(unMembre.getPrenom());
-        int ville = membreAModifier.setVille(unMembre.getVille());
-
-        // Vérifie l'état de la modification (totale, partielle ou refusée)
-        if (date == 1 && lieu == 1 && mail == 1 && mdp == 1 && mdp == 1 && nom == 1 && prenom == 1
-            && ville == 1) {
-          res = 1;
-        } else if (date == 1 || lieu == 1 || mail == 1 || mdp == 1 || mdp == 1 || nom == 1
-            || prenom == 1 || ville == 1) {
-          res = 0;
-        }
+      // Définition du nouveau pseudo du membre
+      codeErreur = membreAModifier.setPseudo(unPseudo);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
       }
+
+      // Définition du nouveau nom du membre
+      codeErreur = membreAModifier.setNom(unNom);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Définition du nouveau prénom du membre
+      codeErreur = membreAModifier.setPrenom(unPrenom);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Définition du nouveau lieu de naissance du membre
+      codeErreur = membreAModifier.setLieuNaissance(unLieuNaissance);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Définition de la nouvelle date de naissance du membre
+      codeErreur = membreAModifier.setDateNaissance(uneDateNaissance);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Définition de la nouvelle ville du membre
+      codeErreur = membreAModifier.setVille(uneVille);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Définition de la nouvelle adresse mail du membre
+      codeErreur = membreAModifier.setMail(unMail);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Définition du nouveau mot de passe du membre
+      codeErreur = membreAModifier.setMotDePasse(unMotDePasse);
+      if (codeErreur != null) {
+        this.codesErreurs.add(codeErreur);
+      }
+
+      // Si une des définitions est un échec, retourne la liste des codes erreurs
+      if (!this.codesErreurs.isEmpty()) {
+        res = this.getCodesErreurs();
+      }
+    } else {
+      this.codesErreurs.add(CodeErreur.MEMBRE_INTROUVABLE);
     }
 
     return res;
   }
 
   /**
-   * Permet de retrouver un membre grâce à son pseudo.
+   * Permet de retrouver un membre grâce à son pseudo {@code unPseudo}.
+   * 
+   * <p>
+   * Si le pseudo {@code unPseudo} n'est pas {@code null}, alors compare un par un le pseudo de
+   * chacun des membres de la liste des membres avec celui passé en paramètre. Le parcours de la
+   * liste s'arrête lorsque le membre ayant le même pseudo à été trouvé et renvoie le
+   * {@code Membre}, ou que la liste a été entièrement parcourue et renvoie {@code null}.
    * 
    * @param unPseudo le pseudo du membre à récupérer
-   * @return le membre correspondant au pseudo, {@code null} sinon
+   * @return l'instance de {@code Membre} correspondant au pseudo, {@code null} sinon
    */
   public Membre getMembre(String unPseudo) {
     Membre membre = null;
@@ -373,7 +529,10 @@ public class Gestion {
       while (!membreTrouve && i < this.listeMembres.size()) {
         if (this.listeMembres.get(i).getPseudo().equals(unPseudo)) {
           membre = this.listeMembres.get(i);
+          membreTrouve = true; // membre trouvé, stoppe le parcours de la liste des membres
         }
+
+        i++; // passe au membre suivant
       }
     }
 
