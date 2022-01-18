bce491b36c4bfcae3dcf0f419bd3c0f795396089
Manon Goasguen <mgoasguen0@gmail.com>
2022-01-18 17:42:32 +0100

    test Evenement : setId setNom setDescriptif setDate

---
+++ CLAMM/src/testsUnitaires/SetDateEvenementTest.java
@@ -0,0 +1,40 @@
+package testsUnitaires;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import gestion.CodeErreur;
+import gestionEvenements.Evenement;
+
+class SetDateEvenementTest {
+
+    Evenement unEvenement;
+
+    @BeforeEach                                         
+    void setUp() {
+      unEvenement = new Evenement();
+    }
+
+    @Test                                               
+    @DisplayName("Test uneDate correcte")   
+    void testCorrecte() {
+        assertEquals(null, unEvenement.setDate(new Date(2022,12,25,19,30)), "Le retour devrait être null");  
+        assertEquals(new Date(2022,12,25,19,30), unEvenement.getDate(), "La date devrait être égale au 25/12/22 19:30 ");  
+    }
+    
+    @Test                                               
+    @DisplayName("Test uneDate passée")   
+    void testPassee() {
+        assertEquals(CodeErreur.DATE_PASSEE, unEvenement.setDate(new Date(2021,12,25,19,30)), "Le retour devrait être un code erreur DATE_PASSEE");  
+        assertEquals(null, unEvenement.getDate(), "La date devrait être égale à null (onchangée) ");  
+    }
+    
+    @Test                                               
+    @DisplayName("Test uneDate nulle")   
+    void testNulle() {
+        assertEquals(CodeErreur.DATE_NULL, unEvenement.setDate(null), "Le retour devrait être un code erreur DATE_NULL");  
+        assertEquals(null, unEvenement.getDate(), "La date devrait être égale à null (onchangée) ");  
+    }
+}
