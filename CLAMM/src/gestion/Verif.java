644141d12884046c9196b05d7684a44d853a62a5
Manon Goasguen <mgoasguen0@gmail.com>
2022-01-24 14:54:54 +0100

    classe Verif liste methode

--- CLAMM/src/gestion/Verif.java
+++ CLAMM/src/gestion/Verif.java
@@ -1,5 +1,19 @@
 package gestion;
 
 public class Verif {
-
+  /*
+  verifIdEvenement
+  verifNomEvenement 
+  verifDescriptifEvenement
+  verifDateEvenement
+  verifLieuEvenement
+  verifNbMaxPersonnesEvenement
+  verifTypeEvenement
+  verifPseudoMembre
+  verifIdentiteMembre (nom, prenom,
+  verifLieuxMembre
+  verifDateMembre
+  verifMailMembre
+  verifMdpMembre
+  */
 }
