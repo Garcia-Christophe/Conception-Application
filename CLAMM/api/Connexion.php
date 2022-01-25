6b031f5968ee1791f458d79a31715f0c656b71c0
Garcia-Christophe <garciachristophe29@gmail.com>
2022-01-25 23:01:53 +0100

    API : connexion ok

--- CLAMM/api/Connexion.php
+++ CLAMM/api/Connexion.php
@@ -23,21 +23,14 @@
         // On récupère le mdp de la bdd dont l'email est le hash de l'email saisi
         // (le mot de passe n'existe pas si la saisie n'est pas le bon email)
         $dbc = BDD_Connexion::getInstance()->getConnexion();
-        $query = "SELECT motDePasse FROM MEMBRE WHERE email = '" . $_POST["email"] . "'";
+        $query = "SELECT motDePasse FROM MEMBRE WHERE mail = '" . $_POST["email"] . "'";
         $stmt = $dbc->query($query);
-
-        if($stmt->num_rows>0){
-            while($row=$stmt->fetch_assoc())
-            {
-                echo $name=$row['mdp'];
-            }
+        $results = $stmt->fetchAll();
+        if (count($results) > 0) {
+            $mdpBDD = $results[0]['motDePasse'];
+        } else {
+            $mdpBDD = "";
         }
-        echo json_encode(array("stmt"=>$stmt->fetch_all()));
-        // if (count($stmt) > 0) {
-        //     $mdpBDD = $stmt[0];
-        // } else {
-        //     $mdpBDD = "";
-        // }
 
         // Vérification de la validité de l'email et du mot de passe
         if ($mdp == $mdpBDD) {
@@ -45,7 +38,5 @@
         }
     }
 
-    echo json_encode(array("email"=>$_POST["email"]));
-    echo json_encode(array("mdp"=>$_POST["mdp"]));
-    // echo json_encode(array("connexion"=>$status));
+    echo json_encode(array("connexion"=>$status));
 ?>
