0b628252b22fab392f3b9c414ff05a69473f8d4c
Garcia-Christophe <garciachristophe29@gmail.com>
2022-01-31 15:33:53 +0100

    tests integration

--- CLAMM/src/testsintegration/testsgestion/TestIntegrationGestionMembres.java
+++ CLAMM/src/testsintegration/testsgestion/TestIntegrationGestionMembres.java
@@ -1,4 +1,4 @@
-package testsintegrations.testsgestion;
+package testsintegration.testsgestion;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
